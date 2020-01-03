c62a668634d4429eb7979a872eb0465d12ab444d
Justin Muniz <justin.muniz@maine.edu>
2020-01-03 00:54:03 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 	// Test if install has already been completed (if completion status file exists)
 		// If it has ben completed, go to Home/index.php
-		header("Location: 127.0.0.1/Home/index.php");
+		header("Location: http://127.0.0.1/Home/index.php");
 		// If it doesn't, go to Install/index.php
 ?>
