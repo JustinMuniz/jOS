ac5bb54f8ee2fe1203894eb2e8f580f3c7a00781
Justin Muniz <justin.muniz@maine.edu>
2020-01-03 16:08:40 -0500

    Update josupdate.c

--- Settings/josupdate.c
+++ Settings/josupdate.c
@@ -1,13 +1,25 @@
 #include <stdlib.h>
 #include <sys/types.h>
-#include <unistd.h>
+#include <stdio.h>
 
 int main() {
 	setuid(0); // for uid to be 0, root
-	char *command = "/usr/bin/git";
-	char *argument1 = "-C";
-	char *argument2 = "/var/www/html";
-	char *argument3 = "pull";
-	execl(command, command, argument1, argument2, argument3, NULL);
+	FILE *shellOutput;
+	char outputCharacter;
+	shellOutput = popen("/usr/bin/git -C /var/www/html pull", "r");
+	if (shellOutput == NULL) {
+		puts("ERROR, unable to open process");
+	}
+	else {
+		while ( (outputCharacter = fgetc(shellOutput)) != EOF ) {
+			putchar(outputCharacter);
+		}
+	}
+	//char *command = "/usr/bin/git";
+	//char *argument1 = "-C";
+	//char *argument2 = "/var/www/html";
+	//char *argument3 = "pull";
+	//execl(command, command, argument1, argument2, argument3, NULL);
+	pclose(shellOutput);
 	return 0; // just to avoid the warning (since never returns)
 }
