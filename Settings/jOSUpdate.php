fe0002c8c0b96c52ace1cfaf1f9e1bb2481907d4
Justin Muniz <justin.muniz@maine.edu>
2020-01-03 03:08:25 -0500

    Update jOSUpdate.php

--- Settings/jOSUpdate.php
+++ Settings/jOSUpdate.php
@@ -1,4 +1,34 @@
 <?php
-	exec("./elevatedCommands 3");
-	exec("./elevatedCommands 9");
+	// Check if there is a rotation lock enabled
+	// If not, rotate the screen to portrait mode
+	exec("../Common/rotateScreen.sh left");
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>jOS Update</title>
+		<link rel = "stylesheet" type = "text/css" href = "../Common/style.css" />
+	</head>
+	<body>
+		<h1>
+			jOS Update
+		</h1>
+		<p>
+			jOS is up to date
+		</p>
+		<a class = "ButtonAnchor" href = "index.php">
+			Settings
+		</a>
+		<a class = "ButtonAnchor" href = "../Home/index.php">
+			Home
+		</a>
+		<p>
+<?php
+	echo exec("./elevatedCommands josupdate");
+	echo exec("./elevatedCommands make");
+?>
+		</p>
+	</body>
+</html>
 ?>
