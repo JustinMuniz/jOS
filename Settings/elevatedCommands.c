ddd1e9f783757848fb2422f2a959b00cdfef01ee
Justin Muniz <justin.muniz@maine.edu>
2020-01-03 02:08:43 -0500

    Update elevatedCommands.c

--- Settings/elevatedCommands.c
+++ Settings/elevatedCommands.c
@@ -5,10 +5,10 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-	if (strcmp(argv[1], "reboot")) {
+	//if (strcmp(argv[1], "reboot")) {
 		setuid(0); // for uid to be 0, root
 		char *command = "/sbin/reboot";
 		execl(command, command, NULL);
 		return 0; // just to avoid the warning (since never returns)
-	}
+//	}
 }
