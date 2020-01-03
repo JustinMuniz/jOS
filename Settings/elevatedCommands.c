510a1f9f9ff996780fd9a19e9988f7ac981470cb
Justin Muniz <justin.muniz@maine.edu>
2020-01-03 02:37:55 -0500

    Update elevatedCommands.c

--- Settings/elevatedCommands.c
+++ Settings/elevatedCommands.c
@@ -16,5 +16,35 @@ int main(int argc, char *argv[]) {
 		execl(command, command, NULL);
 		return 0; // just to avoid the warning (since never returns)
 	}
+	if (strcmp(argv[1], "josupdate")) {
+		char *command = "git pull";
+		execl(command, command, NULL);
+		return 0; // just to avoid the warning (since never returns)
+	}
+	if (strcmp(argv[1], "aptupdate")) {
+		char *command = "/usr/bin/apt-get update";
+		execl(command, command, NULL);
+		return 0; // just to avoid the warning (since never returns)
+	}
+	if (strcmp(argv[1], "softwareupdate")) {
+		char *command = "/usr/bin/apt-get upgrade";
+		execl(command, command, NULL);
+		return 0; // just to avoid the warning (since never returns)
+	}
+	if (strcmp(argv[1], "distupdate")) {
+		char *command = "/usr/bin/apt-get dist-upgrade";
+		execl(command, command, NULL);
+		return 0; // just to avoid the warning (since never returns)
+	}
+	if (strcmp(argv[1], "aptclean")) {
+		char *command = "/usr/bin/apt-get clean";
+		execl(command, command, NULL);
+		return 0; // just to avoid the warning (since never returns)
+	}
+	if (strcmp(argv[1], "aptremove")) {
+		char *command = "/usr/bin/apt-get autoremove --purge";
+		execl(command, command, NULL);
+		return 0; // just to avoid the warning (since never returns)
+	}
 	return 1;
 }
